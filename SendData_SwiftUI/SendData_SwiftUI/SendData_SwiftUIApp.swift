4ca60c87dbe791328c4b6878914ea590724c6761
shvedpavel <pav159208@gmail.com>
2023-12-19 00:41:52 +0300

    created new project

---
+++ SendData_SwiftUI/SendData_SwiftUI/SendData_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendData_SwiftUIApp.swift
+//  SendData_SwiftUI
+//
+//  Created by Apple on 19.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SendData_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
