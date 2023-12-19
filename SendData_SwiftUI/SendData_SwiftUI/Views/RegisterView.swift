a3a9a0a4b3daecf762c0ad53c89bef4bd6db9f16
shvedpavel <pav159208@gmail.com>
2023-12-19 16:09:50 +0300

    добавлено  изменение цвета в зависимости от числа символов

--- SendData_SwiftUI/SendData_SwiftUI/Views/RegisterView.swift
+++ SendData_SwiftUI/SendData_SwiftUI/Views/RegisterView.swift
@@ -8,37 +8,47 @@
 import SwiftUI
 
 struct RegisterView: View {
-   
+    
+//    @Environment(\.colorScheme) var colorScheme: ColorScheme
+  
     @EnvironmentObject var user: UserManager
     @State var name = ""
     
     var body: some View {
-       
+        
         VStack(spacing: 25) {
             
             HStack {
                 TextField("Enter your name", text: $name)
                     .multilineTextAlignment(.center)
                 
-                Text("\(name.count)")
+                if  name.count <= 5 {
+                    Text("\(name.count)")
+                        .foregroundColor(.red)
+                } else {
+                    Text("\(name.count)")
+                        .foregroundColor(.green)
+                }
+                
                 Spacer()
             }
+            
             Button(action: registerUser) {
                 Image(systemName: "checkmark.circle")
                 Text("Ok")
             }
         }
-        
     }
+
     
     private func registerUser() {
-        if !name.isEmpty, name.count <= 5 {
+        if !name.isEmpty, name.count >= 5 {
             user.name = name
             user.isRegister.toggle()
         }
     }
-    
 }
+
 
 #Preview {
     RegisterView()
